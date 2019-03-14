94753094096dfc7b8b9d9a997db024fd79303b69
郭玉朝 <2788828128@qq.com>
2019-03-14 11:12:11 +0800

    增加系统信号监控功能

--- server.go
+++ server.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"net/http"
 	"github.com/gorilla/websocket"
-	"graduation_project_socket/extend/wbskt"
-	"time"
 	"graduation_project_socket/app"
+	"graduation_project_socket/extend/wbskt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
-var(
+
+var (
 	upgrader = websocket.Upgrader{
 		// 允许跨域
-		CheckOrigin:func(r *http.Request) bool{
+		CheckOrigin: func(r *http.Request) bool {
 			return true
 		},
 	}
 )
 
-func wsHandler(w http.ResponseWriter , r *http.Request) {
-	//	w.Write([]byte("hello"))
+// 监听系统信号
+func getSignal(conn *wbskt.Connection, event app.Event, sigs chan os.Signal) {
+	sig := <-sigs
+	switch sig {
+	case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT: // 监听kill、退出信号
+		event.OnClose(conn) // 关闭处理
+		os.Exit(1)
+		break
+	}
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
 	var (
 		wsConn *websocket.Conn
 		err    error
@@ -30,10 +44,15 @@ func wsHandler(w http.ResponseWriter , r *http.Request) {
 	if wsConn, err = upgrader.Upgrade(w, r, nil); err != nil {
 		return
 	}
-
+	sigs := make(chan os.Signal, 1)
 	if conn, err = wbskt.InitConnection(wsConn); err != nil {
 		goto ERR
 	}
+
+	// 监听退出信号
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go getSignal(conn, event, sigs)
+
 	event.OnConn(conn) // 建立连接
 	// 启动线程，发送心跳
 	go func() {
@@ -47,7 +66,6 @@ func wsHandler(w http.ResponseWriter , r *http.Request) {
 			time.Sleep(time.Duration(20) * time.Second)
 		}
 	}()
-
 	// 循环从
 	for {
 		if data, err = conn.ReadMessage(); err != nil {
@@ -59,7 +77,7 @@ ERR:
 	event.OnClose(conn)
 }
 
-func main(){
-	http.HandleFunc("/ws",wsHandler)
-	http.ListenAndServe("0.0.0.0:7777",nil)
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+	http.ListenAndServe("0.0.0.0:7777", nil)
 }
